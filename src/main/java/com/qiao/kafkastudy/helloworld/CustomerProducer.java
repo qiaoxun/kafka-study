b71e819a0b746c87fc8d4373108d0ec0fe0fe24a
Joey <woqiaoxun@163.com>
2022-07-27 22:46:59 +0800

    first commit

---
+++ src/main/java/com/qiao/kafkastudy/helloworld/CustomerProducer.java
@@ -0,0 +1,24 @@
+package com.qiao.kafkastudy.helloworld;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class CustomerProducer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        kafkaProducer.send(new ProducerRecord<>("first", "test"));
+        kafkaProducer.close();
+    }
+
+}
